a7f11e54176a69cd01de036816180edab374f5af
Gaia Casotto <gaia.busacasotto@gmail.com>
2021-12-22 13:11:58 -0800

    restart

--- ChessBoard.h
+++ ChessBoard.h
@@ -13,7 +13,7 @@ private:
     Piece* board[8][8];  //8x8 perche ci vanno ABCDEFG e 12345678
     
 public:
-    ChessBoard();  //costruttore del chessboard
+    ChessBoard(std::string nomeFile);  //costruttore del chessboard
     void printBoard(std::string nomeFile);
     bool isMoveAllowed(std::string position1, std::string position2); //controlla che la mossa sia lecita. Se non lo è, deve rifiutare il comando e chiederne un altro.  //chiama a seconda del pezzo che si trova nella position1, la funzione Move di quel pezzo e vede se può muoversi o no nella position2
     void Move(std::string position1, std::string position2); //move è metodo di scacchiera che chiama il move del pezzo specifico che si trova nella posizione 1.  GIUSTO????
--- replay.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
-#include "ChessBoard.h" //Sfrutto costruttore e print di
-// scacchiera per leggere da file e stampare la mossa
-int main(){
-	return 0;
-}
